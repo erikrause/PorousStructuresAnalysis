e1d215da9f0817906996dfe1af1b3b6eaca546a0
Эркин Камилов <erkimkamilov@gmail.com>
2020-06-28 16:21:47 +0500

    Deleted class diagrams projects. Added ASP.NET Framework WEB MVC

---
+++ NetFrameworkWeb/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace NetFrameworkWeb
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ NetFrameworkWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NetFrameworkWeb.MvcApplication" Language="C#" %>
